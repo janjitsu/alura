2f5d2b46efc5c1857a084e4f4457a99208a0e9a0
Jan Silva <janfrs3@gmail.com>
2021-09-05 20:57:29 -0300

    [KOTLIN SPRING BOOT REST API] Improve error handling

--- kotlin-spring-boot-rest/forum/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ kotlin-spring-boot-rest/forum/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -5,11 +5,11 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoForm(
-    @field:NotEmpty
-    @field:Size(min = 5, max = 100)
+    @field:NotEmpty(message = "titulo não pode ser em branco")
+    @field:Size(min = 5, max = 100, message = "Titulo deve ter entre 5 e 100 caracteres")
     val titulo: String,
 
-    @field:NotEmpty
+    @field:NotEmpty(message = "Mensagem não pode ser em branco")
     val mensagem: String,
 
     @field:NotNull
